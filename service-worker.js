e36faba2f704f635adfc6c49cf4c0083c5617740
noagrp <lihwlee11@gmail.com>
2026-09-14 12:56:02 +0800

    Bump Paint Canvas cache after palette update

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='paint-canvas-v2';
+const CACHE='paint-canvas-v3';
 const SHELL=['./','./index.html','./manifest.webmanifest?v=2','./favicon-32x32.png?v=2','./apple-touch-icon.png?v=2','./icon-192.png?v=2','./icon-512.png?v=2'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
